2084775fc0d5a16a3d5984bf063b1e573304a812
Jakob Grøhn <bokajgd@gmail.com>
2022-05-24 23:47:17 +0200

    setting up structure

---
+++ src/utils.py
@@ -0,0 +1,54 @@
+import numpy as np
+import pandas as pd
+
+# Define W(d, l) - Function that calculates impending corrosion speed - also based on y.
+def Q(d, l):
+    """Function for determining rate of corrosion
+
+    Args:
+        d (int): Difference in grayness
+        l (float): scaling factor
+    """
+    return (255 - d) * l
+
+
+# Function for defining context kernel
+def context(seed: np.ndarray, r: int, c: int):
+    """Defines a list of all cells neigbouring a given cell
+
+    Args:
+        seed (np.ndarray): 2D array of image
+        r (int): Row number of the given cell
+        c (int): Collumn number of the given cell
+
+    Returns:
+        List: List of values for the 9 cells
+    """
+    return [
+        seed[r, c],
+        seed[r, c + 1],
+        seed[r, c + 2],
+        seed[r + 1, c],
+        seed[r + 1, c + 2],
+        seed[r + 2, c],
+        seed[r + 2, c + 1],
+        seed[r + 2, c + 2],
+    ]
+
+
+# Define function for binarising MNIST images
+def binarise(img, threshold):
+    """Binarizes a 2D image on the basis of a threshold (dead cells = 0, alive cells = 1)
+
+    Args:
+        img (np.ndarray): 2D array to binarize
+        threshold (int): Threshold for binarizing upon
+
+    Returns:
+        np.ndarray: Binarized image
+    """
+
+    img[img >= threshold] = 255
+    img[img < threshold] = 0
+
+    return img
