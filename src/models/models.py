f678a5418a83d20a4a429eda7b87c3c55e028170
Jakob Grøhn <bokajgd@gmail.com>
2022-05-25 10:54:22 +0200

    lr model

--- src/models/models.py
+++ src/models/models.py
@@ -2,9 +2,7 @@ import keras
 import pandas as pd
 from sklearn import metrics
 
-# Defining function for 2D CNN for raw MNIST data
-
-# Defining function for 1D CNN for timeseries classification
+# Defining function for CNN models
 def cnn(X_train, y_train, X_test, y_test, model, epochs):
     """Trains and tests a given 1D CNN model
 
@@ -36,6 +34,40 @@ def cnn(X_train, y_train, X_test, y_test, model, epochs):
 
     return predictions, classification_report
 
+
 # Defining logistic regression for raw MNIST data
+def lr(X_train, y_train, X_test, y_test):
+    """Trains and tests a logistic regression. May take 2D or 1D features
+
+    Args:
+        X_train (array): Training set with dim = (n_samples, 1st_dim) or dim = (n_samples, 1st_dim, 2nd_dim)
+        y_train (array): Training labels with dim = (n_samples)
+        X_test (array): Testing set with dim = (n_samples, 1st_dim) or dim = (n_samples, 1st_dim, 2nd_dim)
+        y_test (array): Testing labels with dim = (n_samples)
+
+    Returns:
+        tuple: Tuple containing predictions (in probabilities) and a classification report
+    """    
+
+    # If the array is 2D, flatten to 1D
+    if len(X_train.shape) > 2:
+        X_train = X_train.reshape(X_train.shape[0], -1)
+    if len(X_test.shape) > 2:
+        X_test = X_test.reshape(X_test.shape[0], -1)
+    
+    # Define model
+    model = LogisticRegression(max_iter=250)
+
+    # Fit model
+    model.fit(X_train, y_train)
+
+    # Make predictions (and get outcome in probabilities)
+    predictions = model.predict_proba(X_test)
+
+    # Make classification report
+    classification_report = pd.DataFrame.from_dict(metrics.classification_report(y_test, np.argmax(predictions, axis=1), output_dict=True))
+
+    return predictions, classification_report
+
 
 # Defining ensemble output functions
