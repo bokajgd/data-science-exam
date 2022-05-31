fe9251e8bf6c20ab77d3862755720a9814b5c117
Jakob Grøhn <bokajgd@gmail.com>
2022-05-31 11:52:37 +0200

    reworking some lines

--- src/main.py
+++ src/main.py
@@ -82,6 +82,32 @@ def main():
                                               n_generations=n_gens,
                                               s=s,
                                               context=context)
+
+    # Generate features on test set
+    df_cor_test, augmented_numbers_cor_test, cor_mass_test = cumulative_change(X=X_test, 
+                                              y=y_test, 
+                                              rule='corrosion',
+                                              n_generations=int(n_gens/2),
+                                              Q=Q, 
+                                              l=l,
+                                              v=v,
+                                              context=context)
+
+    df_gol_test, augmented_numbers_gol_test, gol_change_test = cumulative_change(X=X_test, 
+                                              y=y_test, 
+                                              rule='gol',
+                                              n_generations=int(n_gens/5),
+                                              threshold=0.0,
+                                              context=context)
+
+    df_melt_test, augmented_numbers_melt_test, melt_mass_test = cumulative_change(X=X_test, 
+                                              y=y_test, 
+                                              rule='melt',
+                                              n_generations=n_gens,
+                                              s=s,
+                                              context=context)
+
+
     
     # Make iterations 3D to arrays
     augmented_numbers_cor = np.array(augmented_numbers_cor)
@@ -90,6 +116,9 @@ def main():
     augmented_numbers_cor_val = np.array(augmented_numbers_cor_val)
     augmented_numbers_gol_val = np.array(augmented_numbers_gol_val)
     augmented_numbers_melt_val = np.array(augmented_numbers_melt_val)
+    augmented_numbers_cor_test = np.array(augmented_numbers_cor_test)
+    augmented_numbers_gol_test = np.array(augmented_numbers_gol_test)
+    augmented_numbers_melt_test = np.array(augmented_numbers_melt_test)
 
         # Save files
     with open('/Users/jakobgrohn/Desktop/Cognitive_Science/Cognitive Science 8th Semester/Data Science/Eksamen/data-science-exam/output/cor_aug.csv', 'w', newline='') as csvfile:
@@ -104,76 +133,76 @@ def main():
         writer = csv.writer(csvfile, delimiter=',')
         writer.writerows(augmented_numbers_melt)
 
-    with open('/Users/jakobgrohn/Desktop/Cognitive_Science/Cognitive Science 8th Semester/Data Science/Eksamen/data-science-exam/output/cor_aug_val.csv', 'w', newline='') as csvfile:
+    with open('/Users/jakobgrohn/Desktop/Cognitive_Science/Cognitive Science 8th Semester/Data Science/Eksamen/data-science-exam/output/cor_aug_test.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',')
-        writer.writerows(augmented_numbers_cor_val)
+        writer.writerows(augmented_numbers_cor_test)
     
-    with open('/Users/jakobgrohn/Desktop/Cognitive_Science/Cognitive Science 8th Semester/Data Science/Eksamen/data-science-exam/output/gol_aug_val.csv', 'w', newline='') as csvfile:
+    with open('/Users/jakobgrohn/Desktop/Cognitive_Science/Cognitive Science 8th Semester/Data Science/Eksamen/data-science-exam/output/gol_aug_test.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',')
-        writer.writerows(augmented_numbers_gol_val)
+        writer.writerows(augmented_numbers_gol_test)
 
-    with open('/Users/jakobgrohn/Desktop/Cognitive_Science/Cognitive Science 8th Semester/Data Science/Eksamen/data-science-exam/output/melt_aug_val.csv', 'w', newline='') as csvfile:
+    with open('/Users/jakobgrohn/Desktop/Cognitive_Science/Cognitive Science 8th Semester/Data Science/Eksamen/data-science-exam/output/melt_aug_test.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',')
-        writer.writerows(augmented_numbers_melt_val)
+        writer.writerows(augmented_numbers_melt_test)
 
     
     ### RUN MODELS ###
 
     # Concatenating using augmented images
     X_train_flat = X_train.reshape(2000,784)
-    X_val_flat = X_val.reshape(1000,784)
+    X_test_flat = X_test.reshape(1000,784)
 
-    lr_mnist_preds, lr_mnist_report = lr(X_train, y_train, X_val, y_val)
-    lr_mnist_large_preds, lr_mnist_large_report = lr(X_train_large, y_train_large, X_val, y_val)
+    lr_mnist_preds, lr_mnist_report = lr(X_train, y_train, X_test, y_test)
+    lr_mnist_large_preds, lr_mnist_large_report = lr(X_train_large, y_train_large, X_test, y_test)
 
     last_aug_cor = np.array(augmented_numbers_cor)[:, 30, :,: ]
     last_aug_cor_flat = last_aug_cor.reshape(2000,784)
-    last_aug_cor_val = np.array(augmented_numbers_cor_val)[:, 30, :,: ]
-    last_aug_cor_val_flat = last_aug_cor_val.reshape(1000,784)
+    last_aug_cor_test = np.array(augmented_numbers_cor_test)[:, 30, :,: ]
+    last_aug_cor_test_flat = last_aug_cor_test.reshape(1000,784)
 
     X_train_aug_cor_con = np.hstack((X_train_flat, last_aug_cor_flat))
-    X_val_aug_cor_con = np.hstack((X_val_flat, last_aug_cor_val_flat))
+    X_test_aug_cor_con = np.hstack((X_test_flat, last_aug_cor_test_flat))
 
-    lr_cor_aug_preds, lr_cor_aug_report = lr(last_aug_cor_flat, y_train, last_aug_cor_val_flat, y_val)
-    lr_cor_aug_con_preds, lr_cor_aug_con_report = lr(X_train_aug_cor_con, y_train, X_val_aug_cor_con, y_val)
+    lr_cor_aug_preds, lr_cor_aug_report = lr(last_aug_cor_flat, y_train, last_aug_cor_test_flat, y_test)
+    lr_cor_aug_con_preds, lr_cor_aug_con_report = lr(X_train_aug_cor_con, y_train, X_test_aug_cor_con, y_test)
 
     last_aug_melt = np.array(augmented_numbers_melt)[:, 60, :,: ]
     last_aug_melt_flat = last_aug_melt.reshape(2000,784)
-    last_aug_melt_val = np.array(augmented_numbers_melt_val)[:, 60, :,: ]
-    last_aug_melt_val_flat = last_aug_melt_val.reshape(1000,784)
+    last_aug_melt_test = np.array(augmented_numbers_melt_test)[:, 60, :,: ]
+    last_aug_melt_test_flat = last_aug_melt_test.reshape(1000,784)
 
     X_train_aug_melt_con = np.hstack((X_train_flat, last_aug_melt_flat))
-    X_val_aug_melt_con = np.hstack((X_val_flat, last_aug_melt_val_flat))
+    X_test_aug_melt_con = np.hstack((X_test_flat, last_aug_melt_test_flat))
 
-    lr_melt_aug_preds, lr_melt_aug_report = lr(last_aug_melt_flat, y_train, last_aug_melt_val_flat, y_val)
-    lr_melt_aug_con_preds, lr_melt_aug_con_report = lr(X_train_aug_melt_con, y_train, X_val_aug_melt_con, y_val)
+    lr_melt_aug_preds, lr_melt_aug_report = lr(last_aug_melt_flat, y_train, last_aug_melt_test_flat, y_test)
+    lr_melt_aug_con_preds, lr_melt_aug_con_report = lr(X_train_aug_melt_con, y_train, X_test_aug_melt_con, y_test)
 
     last_aug_gol = np.array(augmented_numbers_gol)[:, 12, :,: ]
     last_aug_gol_flat = last_aug_gol.reshape(2000,784)
-    last_aug_gol_val = np.array(augmented_numbers_gol_val)[:, 12, :,: ]
-    last_aug_gol_val_flat = last_aug_gol_val.reshape(1000,784)
+    last_aug_gol_test = np.array(augmented_numbers_gol_test)[:, 12, :,: ]
+    last_aug_gol_test_flat = last_aug_gol_test.reshape(1000,784)
 
     X_train_aug_gol_con = np.hstack((X_train_flat, last_aug_gol_flat))
-    X_val_aug_gol_con = np.hstack((X_val_flat, last_aug_gol_val_flat))
+    X_test_aug_gol_con = np.hstack((X_test_flat, last_aug_gol_test_flat))
 
-    lr_gol_aug_preds, lr_gol_aug_report = lr(last_aug_gol_flat, y_train, last_aug_gol_val_flat, y_val)
-    lr_gol_aug_con_preds, lr_gol_aug_con_report = lr(X_train_aug_gol_con, y_train, X_val_aug_gol_con, y_val)
+    lr_gol_aug_preds, lr_gol_aug_report = lr(last_aug_gol_flat, y_train, last_aug_gol_test_flat, y_test)
+    lr_gol_aug_con_preds, lr_gol_aug_con_report = lr(X_train_aug_gol_con, y_train, X_test_aug_gol_con, y_test)
 
     X_train_aug_all_con = np.hstack((X_train_flat, last_aug_melt_flat, last_aug_cor_flat, last_aug_gol_flat))
-    X_val_aug_all_con = np.hstack((X_val_flat, last_aug_melt_val_flat, last_aug_cor_val_flat, last_aug_gol_val_flat))
+    X_test_aug_all_con = np.hstack((X_test_flat, last_aug_melt_test_flat, last_aug_cor_test_flat, last_aug_gol_test_flat))
 
-    lr_all_aug_con_preds, lr_all_aug_con_report = lr(X_train_aug_all_con, y_train, X_val_aug_all_con, y_val)
+    lr_all_aug_con_preds, lr_all_aug_con_report = lr(X_train_aug_all_con, y_train, X_test_aug_all_con, y_test)
 
     # Plotting MCC
-    mcc_lr_mnist = metrics.matthews_corrcoef(y_val, np.argmax(lr_mnist_preds, axis=1), sample_weight=None)
-    mcc_lr_mnist_large = metrics.matthews_corrcoef(y_val_large, np.argmax(lr_mnist_large_preds, axis=1), sample_weight=None)
-    mcc_lr_melt = metrics.matthews_corrcoef(y_val, np.argmax(lr_melt_aug_preds, axis=1), sample_weight=None)
-    mcc_lr_cor = metrics.matthews_corrcoef(y_val, np.argmax(lr_cor_aug_preds, axis=1), sample_weight=None)
-    mcc_lr_gol = metrics.matthews_corrcoef(y_val, np.argmax(lr_gol_aug_con_preds, axis=1), sample_weight=None)
-    mcc_lr_mnist_melt = metrics.matthews_corrcoef(y_val, np.argmax(lr_melt_aug_con_preds, axis=1), sample_weight=None)
-    mcc_lr_mnist_cor = metrics.matthews_corrcoef(y_val, np.argmax(lr_cor_aug_con_preds, axis=1), sample_weight=None)
-    mcc_lr_mnist_gol = metrics.matthews_corrcoef(y_val, np.argmax(lr_gol_aug_con_preds, axis=1), sample_weight=None)
-    mcc_lr_all_con = metrics.matthews_corrcoef(y_val, np.argmax(lr_all_aug_con_preds, axis=1), sample_weight=None)
+    mcc_lr_mnist = metrics.matthews_corrcoef(y_test, np.argmax(lr_mnist_preds, axis=1), sample_weight=None)
+    mcc_lr_mnist_large = metrics.matthews_corrcoef(y_test_large, np.argmax(lr_mnist_large_preds, axis=1), sample_weight=None)
+    mcc_lr_melt = metrics.matthews_corrcoef(y_test, np.argmax(lr_melt_aug_preds, axis=1), sample_weight=None)
+    mcc_lr_cor = metrics.matthews_corrcoef(y_test, np.argmax(lr_cor_aug_preds, axis=1), sample_weight=None)
+    mcc_lr_gol = metrics.matthews_corrcoef(y_test, np.argmax(lr_gol_aug_con_preds, axis=1), sample_weight=None)
+    mcc_lr_mnist_melt = metrics.matthews_corrcoef(y_test, np.argmax(lr_melt_aug_con_preds, axis=1), sample_weight=None)
+    mcc_lr_mnist_cor = metrics.matthews_corrcoef(y_test, np.argmax(lr_cor_aug_con_preds, axis=1), sample_weight=None)
+    mcc_lr_mnist_gol = metrics.matthews_corrcoef(y_test, np.argmax(lr_gol_aug_con_preds, axis=1), sample_weight=None)
+    mcc_lr_all_con = metrics.matthews_corrcoef(y_test, np.argmax(lr_all_aug_con_preds, axis=1), sample_weight=None)
 
     print(mcc_lr_mnist)
     print(mcc_lr_mnist_large)
@@ -216,7 +245,7 @@ def main():
     lr_mnist_wrong_true_false = preds_true_false(y_val, lr_mnist_preds)
     
     wrong_predictions = wrong_preds(lr_mnist_wrong_true_false,lr_all_aug_con_true_false)
-    # Number 23 is incorrectly classified
+    #print(wrong_predictions) # Number 23 is incorrectly classified
 
     cor_ex = np.array(augmented_numbers_cor_val)[23, 30, : , :]
     plot_images(cor_ex)
@@ -265,15 +294,14 @@ def main():
 
 
     # Plot images of 8 iterations 
-    cor_first_8 = augmented_numbers_cor[0, 0:8, : , :]
+    cor_first_8 = np.array(augmented_numbers_cor)[0, 0:9, : , :]
     plot_8_generations(cor_first_8)
 
-    gol_first_8 = augmented_numbers_gol[0, 0:8, : , :]
+    gol_first_8 = augmented_numbers_gol[0, 0:9, : , :]
     plot_8_generations(gol_first_8)
 
-    melt_first_8 = augmented_numbers_melt[0, 0:8, : , :]
+    melt_first_8 = augmented_numbers_melt[0, 0:9, : , :]
     plot_8_generations(melt_first_8)
-
 
 
 if __name__ == "__main__":
